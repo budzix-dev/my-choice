7aa78641cd1ca8e0b019a4a9212389f1129f2f84
budzix-dev <pbudziak.github@gmail.com>
2023-03-06 20:46:55 +0100

    finished1

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,38 @@
 <script setup lang="ts">
-import traits from "~~/data/traits";
-import skills from "~~/data/skills";
+import traits from "~~/utils/traits";
+import skills from "~~/utils/skills";
+import interests from "~~/utils/interests";
+
+const route = useRoute();
+
+onscroll = () => {
+  if (process.client) {
+    const scrollBtn = document.getElementById("scroll-btn");
+    if (window.scrollY > 100) {
+      scrollBtn!.classList.remove("hidden");
+    } else {
+      scrollBtn!.classList.add("hidden");
+    }
+
+    const prevBtn = document.getElementById("prev-btn");
+    if (route.hash !== "") {
+      prevBtn!.classList.remove("hidden");
+    } else {
+      prevBtn!.classList.add("hidden");
+    }
+  }
+};
+
+const sections = [
+  "#",
+  "#what-why",
+  "#about-programming",
+  "#traits-and-skills",
+  "#useful-interests",
+  "#how-to-get-there",
+  "#swot-analysis",
+  "#for-and-against",
+];
 </script>
 
 <template>
@@ -56,7 +88,7 @@ import skills from "~~/data/skills";
     </Hero>
     <Hero id="about-programming" img-src="/images/chalkboard.jpg">
       <div>
-        <div class="grid space-y-10 border p-7">
+        <div class="grid space-y-5 lg:space-y-10 border p-7">
           <h1 class="text-5xl xl:text-6xl font-medium">Co robi programista?</h1>
           <div class="text-lg xl:text-xl text-left">
             Oczywistą odpowiedzią jest:
@@ -176,13 +208,184 @@ import skills from "~~/data/skills";
         </a>
       </div>
     </Hero>
-    <Hero id="useful-interests">
+    <Hero id="useful-interests" img-src="/images/blackboard.jpg">
       <div>
-        <h1 class="text-3xl">
-          Znając już przydatne umiejętności, oto parę przydatnych zainteresowań,
-          które pomogą te umiejętności rozwinąć.
-        </h1>
+        <div
+          class="text-white max-w-3xl grid space-y-10 border p-5 rounded-md bg-base-100/40"
+        >
+          <h1 class="text-3xl font-medium">
+            Zainteresowania, które mogą pomóc w pracy programisty:
+          </h1>
+          <div class="grid space-y-4">
+            <div
+              class="rounded-lg border p-5 flex bg-secondary/40 items-center h-fit"
+              v-for="interest in interests"
+            >
+              <div class="ml-5">
+                <p class="text-lg text-left">
+                  <span class="font-bold">{{ interest.name }}</span> -
+                  {{ interest.description }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a
+          class="btn btn-secondary text-white btn-md rounded-md px-5 gap-1 mt-2"
+          href="#how-to-get-there"
+          >Jak zostać programistą?
+          <Icon name="bx:down-arrow-alt" size="15" />
+        </a>
       </div>
     </Hero>
+    <Hero id="how-to-get-there" img-src="images/graybg.jpg">
+      <div>
+        <div class="grid space-y-5 bg-base-100/40 rounded-md p-7">
+          <h1 class="text-4xl">Jaką ścieżkę obrać by zostać programistą?</h1>
+          <p class="text-lg">
+            Jest wiele takich ścieżek, by pracować jako programista
+            <span class="font-medium">nie jest</span> konieczne posiadanie
+            wykształcenia. Oczywiście, pomocne jest ono w znalezieniu pracy,
+            jednak zwykle nie jest kluczowe w procesie rekrutacji. Wielu
+            programistów jest
+            <span class="italic font-medium">self-taught</span>, czyli uczy się
+            samodzielnie, korzystając z internetowych oraz książkowych źródeł.
+          </p>
+          <p class="text-lg">
+            W Polsce jedną z popularniejszych uczelni jest Politechnika
+            Wrocławska. Wśród jej kierunków znajdujeą się kierunki związane z
+            programowaniem, takie jak automatyka i robotyka, czy
+            cyberbezpieczeństwo. Dla tych kierunków najbardziej punktowanymi
+            przedmiotami są matematyka, przedmiot dodatkowy
+            (fizyka/informatyka), a niżej punktowane są język obcy i język
+            polski. Wysokość opłaty rekrutacyjnej dla tych kierunków wynosi po
+            <span class="whitespace-nowrap">85 zł</span>. Informacje o terminach
+            rekrutacji i składania dokumentów znajdziesz
+            <a
+              href="https://www.otouczelnie.pl/uczelnia_dzial/4376/Politechnika-Wroclawska-rekrutacja-na-studia-20212022"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="link-secondary whitespace-nowrap"
+              >tutaj</a
+            >.
+          </p>
+          <p class="text-lg">
+            Politechnika Wrocławska jest jedną z najbardziej renomowanych
+            uczelni w Polsce.
+            <span class="italic">
+              Trzecia pozycja wśród uczelni technicznych, szóste miejsce w
+              ogólnej klasyfikacji oraz tradycyjnie pierwsza lokata na Dolnym
+              Śląsku - to wyniki Politechniki Wrocławskiej w rankingu szkół
+              wyższych Fundacji Perspektywy 2020.
+            </span>
+            Bierze ona udział w wielu programach, jak Erasmus czy Student
+            Exchange i więcej. Istnieje również program stypedium dla studentów
+            <span class="italic font-medium whitespace-nowrap"
+              >Wybitnie uzdolnieni na Politechnice Wrocławskiej</span
+            >. Informacje o akademikach można znaleźć na
+            <a
+              href="http://drugidom.pwr.edu.pl"
+              target="_blank"
+              class="link-info"
+              >tej stronie</a
+            >.
+          </p>
+          <p class="text-lg">
+            Więcej informacji można znaleźć na stronie
+            <a
+              href="https://www.rekrutacja.pwr.edu.pl/"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="link-accent whitespace-nowrap"
+              >Politechniki Wrocławskiej</a
+            >.<br />Istnieje oczywiście więcej uczelni w Polsce, które oferują
+            kierunki związane z programowaniem, jak np. Politechnika Warszawska
+            lub Politechnika Gdańska.
+          </p>
+          <p class="text-lg">
+            Po zdobyciu odpowiednich umiejętności, można rozpocząć pracę w
+            branży. Pracować jako programista można w dzisiejszych czasach w
+            praktycznie każdej większej firmie, największe z nich to np. Google,
+            Microsoft czy Apple. Można również pracować w mniejszych, lecz
+            bardziej wyspecjalizowanych firmach. Innymi kierunkami jest
+            założenie własnego start-up lub freelancing. W każdym razie -
+            zapotrzebowanie na programistów jest ogromne, a wciąż ono rośnie.
+          </p>
+        </div>
+        <a
+          class="btn btn-primary text-white w-full rounded-md px-5 gap-1 mt-2"
+          href="#swot-analysis"
+          >Analiza SWOT<Icon name="bx:down-arrow-alt" size="15"
+        /></a>
+      </div>
+    </Hero>
+    <Hero id="swot-analysis" img-src="/images/bg2.jpg">
+      <div>
+        <div class="border lg:p-20 p-5 rounded-t-xl bg-base-100/80 max-w-7xl">
+          <div class="text-left text-white">
+            <h4 class="text-4xl mb-5 text-left">Analiza SWOT</h4>
+            <p class="text-2xl text-left">
+              Jest to analiza, która może pomóc w dokonaniu wyboru. Tu jest moja
+              osobista analiza:
+            </p>
+          </div>
+          <div class="mt-7">
+            <img src="images/swot.jpeg" alt="swot" class="w-full rounded-md" />
+          </div>
+        </div>
+        <a
+          class="btn text-white w-full rounded-b-md px-5 gap-1 bg-base-300/90"
+          href="#for-and-against"
+          >Wskazania i przeciwwskazania<Icon name="bx:down-arrow-alt" size="15"
+        /></a>
+      </div>
+    </Hero>
+    <Hero id="for-and-against" img-src="/images/bg3.jpg">
+      <div>
+        <div class="bg-base-100/70 rounded-lg p-7 text-white">
+          <h1 class="text-6xl">Wskazania i przeciwwskazania</h1>
+        </div>
+        <div class="flex space-x-10 mt-10 justify-between">
+          <div class="bg-base-100/70 rounded-lg p-7 text-white w-1/2">
+            <Icon name="typcn:tick" size="100" />
+            <ul class="list-disc ml-5 text-xl mt-5">
+              <li>
+                Zręczność w obsłudze sprzętu komputerowego, takiego jak myszka,
+                klawiatura, monitor, etc.
+              </li>
+            </ul>
+          </div>
+          <div class="bg-base-100/70 rounded-lg p-7 text-white w-1/2">
+            <Icon name="typcn:times" size="100" />
+            <ul class="list-disc ml-5 text-xl mt-5">
+              <li>
+                Posiadanie wad fizycznych lub zdrowotnych uniemożliwiających
+                zbyt długą pracę przy komputerze.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Hero>
+    <div
+      class="sticky w-full flex justify-end bottom-0 pb-3 lg:pb-10 pr-5 lg:pr-16 transition hidden"
+      id="scroll-btn"
+    >
+      <a href="#" class="btn btn-secondary btn-circle lg:btn-lg xl:btn-xl">
+        <Icon name="bx:up-arrow-alt" size="25" />
+      </a>
+    </div>
+
+    <div
+      class="sticky w-full flex justify-start bottom-0 pb-3 lg:pb-10 pl-5 lg:pl-16 transition hidden"
+      id="prev-btn"
+    >
+      <a
+        :href="sections[sections.indexOf(route.hash) - 1]"
+        class="btn btn-secondary btn-circle lg:btn-lg xl:btn-xl gap-1"
+      >
+        <Icon name="bx:left-arrow-alt" size="25" />
+      </a>
+    </div>
   </main>
 </template>
---
+++ utils/interests.ts
@@ -0,0 +1,28 @@
+export default [
+  {
+    name: "Gry logiczne",
+    description: `
+            Gry logiczne pozwolają na rozwój umiejętności analitycznego myślenia.
+            `,
+  },
+  {
+    name: "Nauka języka angielskiego",
+    description: `
+            Nauka języka angielskiego pozwala na szybsze i łatwiejsze zdobywanie wiedzy związanej z branżą IT,
+            ponieważ większość materiałów jest napisana w tym języku.
+            `,
+  },
+  {
+    name: "Komputery i nowe technologie",
+    description: `
+            Takie zainteresowania pozwalają na bycie na bieżąco z tym, co dzieje się w świecie IT,
+            co powiązane jest z pracą programisty.
+            `,
+  },
+  {
+    name: "Matematyka",
+    description: `
+            Matematyka używana jest w wielu dziedzinach informatykim, a także programowania.
+            Rozwija ona umiejętność logicznego myślenia.`,
+  },
+];
